d7c58584f487c57dfcd8ca549dbf4cf1c7647c7f
Ábner Neves <abnermneves@gmail.com>
2019-09-28 17:25:27 -0300

    leitura de entrada completa. falta implementar commander

--- tp1/program/main.cpp
+++ tp1/program/main.cpp
@@ -3,24 +3,41 @@
 #include "grafo.h"
 
 int main(){
-  unsigned int n, m, a, b;
+  unsigned int n, m, k, a, b;
+  char c;
   Vertice* v;
-  std::cin >> n >> m;
+  std::cin >> n >> m >> k;
   Grafo* g = new Grafo();
+
+  //lê os n vértices
   for (unsigned int i = 0; i < n; i++){
     std::cin >> a;
     v = new Vertice(i+1, a);
     g->addVertice(v);
   }
+
+  //lê as m arestas
   for (unsigned int i = 0; i < m; i++){
     std::cin >> a >> b;
     g->addAresta(a, b);
   }
 
-  g->meeting();
-  g->swap(4, 2);
-  g->swap(3, 6);
-  g->swap(4, 7);
-  g->imprimirGrafo();
+  //lê as k instruções
+  for (unsigned int i = 0; i < k; i++){
+    std::cin >> c;
+    switch(c){
+      case 'S':
+        std::cin >> a >> b;
+        g->swap(a, b);
+        break;
+      case 'C':
+        std::cin >> a;
+        break;
+      case 'M':
+        g->meeting();
+        break;
+    }
+  }
+  //g->imprimirGrafo();
   return 0;
 }
