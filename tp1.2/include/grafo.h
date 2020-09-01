e3b4c30a9ccdeefdb60907e38449370a68a7c33c
Ábner Neves <abnermneves@gmail.com>
2020-09-01 15:34:12 -0300

    verificação de índices corrigida. não dá mais a volta no tabuleiro

--- tp1.2/include/grafo.h
+++ tp1.2/include/grafo.h
@@ -6,8 +6,7 @@
 
 class Grafo {
 private:
-    unsigned int n; //quantidade de vértices
-    unsigned int m; //quantidade de arestas
+    unsigned int n, m; //dimensões do tabuleiro
     std::vector<std::list<int>*> vertices; //vector de lists para as adjacências
 public:
     Grafo(unsigned int n, unsigned int m, unsigned int** tabuleiro);
@@ -15,6 +14,8 @@ public:
 
     //converte os índices da matriz para uma indexação linear
     unsigned int index(int i, int j);
+    //verifica se os índices são válidos
+    bool indicesValidos(int i, int j);
     //imprime grafo
     void imprimir();
 };
--- tp1.2/src/grafo.cpp
+++ tp1.2/src/grafo.cpp
@@ -9,18 +9,32 @@ Grafo::Grafo(unsigned int n, unsigned int m, unsigned int** tabuleiro){
     unsigned int v, x, y, mx, my;
     for (unsigned int i = 0; i < n; i++){
         for (unsigned int j = 0; j < m; j++){
-            //convertendo os índices da matriz para uma indexação linear
             v = index(i, j); //vértice atual
-            x = index(i, j+1); //vizinho na direção +x
-            y = index(i-1, j); //vizinho na direção y
-            mx = index(i, j-1); //vizinho na direção -x
-            my = index(i+1, j); //vizinho na direção -y
-
             this->vertices.push_back(new std::list<int>);
-            this->vertices.at(v)->push_back(x);
-            this->vertices.at(v)->push_back(y);
-            this->vertices.at(v)->push_back(mx);
-            this->vertices.at(v)->push_back(my);
+
+            //vizinho na direção +x
+            if (indicesValidos(i, j+1)){
+                x = index(i, j+1);
+                this->vertices.at(v)->push_back(x);
+            }
+
+            //vizinho na direção +y
+            if (indicesValidos(i-1, j)){
+                y = index(i-1, j);
+                this->vertices.at(v)->push_back(y);
+            }
+
+            //vizinho na direção -x
+            if (indicesValidos(i, j-1)){
+                mx = index(i, j-1);
+                this->vertices.at(v)->push_back(mx);
+            }
+
+            //vizinho na direção -y
+            if (indicesValidos(i+1, j)){
+                my = index(i+1, j);
+                this->vertices.at(v)->push_back(my);
+            }
         }
     }
 }
@@ -30,20 +44,14 @@ Grafo::~Grafo(){
 }
 
 unsigned int Grafo::index(int i, int j){
-    //como o módulo de C++ não é euclidiano,
-    //acabaria retornando um valor inválido para operações com -1
-    //então precisa colocar essas verificações
-    if (i == -1)
-        i = this->n - 1;
-    else
-        i = i % this->n;
-
-    if (j == -1)
-        j = this->m - 1;
-    else
-        j = j % this->m;
-
     return this->m*i + j;
+}
+
+bool Grafo::indicesValidos(int i, int j){
+    //se os índices estão fora do tabuleiro, retorna false
+    if (i < 0 || i >= (int)this->n || j < 0 || j >= (int)this->m)
+        return false;
+    return true;
 }
 
 void Grafo::imprimir(){
